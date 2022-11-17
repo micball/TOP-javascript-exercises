09d5c78428be2baa63ff03338c24a29a899d6586
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 17:26:52 -0700

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let final = "";
+    let temp = string.split('');
+    for (let i = 0; i < temp.length; i++) {
+        final = temp[i]+final;
+    }
+    return final;
 };
 
 // Do not edit below this line
