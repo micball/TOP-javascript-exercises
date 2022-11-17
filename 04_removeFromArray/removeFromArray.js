cfec337f1900619093b9caa174dd0a6cb0f1e2e9
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 17:50:51 -0700

    complete challenge 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    let tempArr = array.slice();
+    for (let j = 0; j < theArgs.length; j++){
+        for (i=0; i < tempArr.length; i++){
+            if (tempArr[i] === theArgs[j]) {
+            tempArr.splice(i, 1);
+            }
+        }
+    }
+    return tempArr;
 };
 
 // Do not edit below this line
