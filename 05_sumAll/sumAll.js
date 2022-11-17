d5bd43158703a821e6dc52435c078abe4e8f9baa
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 18:28:11 -0700

    complete challenge 04

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let temp = [];
+    let sum = 0;
+    if (numOne < 0 || numTwo < 0 || typeof numOne != 'number' || typeof numTwo != 'number') {
+        return "ERROR";
+    }
+    if (numOne > numTwo){
+        for (let i = numTwo; i <= numOne; i++){
+            temp.push(i);
+        }
+    } else if (numTwo > numOne) {
+        for (let i = numOne; i <= numTwo; i++){
+            temp.push(i);
+        }
+    } else if (numOne == numTwo) {
+        return numOne+numTwo;
+    } else if (numTwo == undefined) {
+        return numOne;
+    }
 
+    for (let i = 0; i < temp.length; i++) {
+        sum += temp[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
