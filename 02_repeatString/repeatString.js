c98f9988282e46e702ba4b887c0e69038d7496e1
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 17:20:36 -0700

    complete challenge 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if (num >= 0 ){
+        for (let i = 0; i < num; i++){
+            finalString+=string;
+        }
+    return finalString;
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
