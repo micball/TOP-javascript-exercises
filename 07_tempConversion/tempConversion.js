580a9414a40bcc6406b98a6ddcecadb3aaec0f63
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 19:22:18 -0700

    Complete challenge 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return (Math.round(10*(temp-32)*(5/9)))/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return (Math.round(10*(temp*(9/5)+32)))/10;
 };
 
+
+//Cels to Far formula: (temp*(9/5)+32)
+//Far to Cels formula: (temp-32)*(5/9)
 // Do not edit below this line
 module.exports = {
   ftoc,
