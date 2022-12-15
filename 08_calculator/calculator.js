f16702a7022ded31d2ce25a88783905b30395bd9
Michael Ballif <mballifcodes@gmail.com>
2022-12-14 23:01:50 -0700

    compelete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+// maximum 2 inputs
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumAll = arr.reduce((total, index) => {
+    return total + index;
+  },
+  0 )
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, nextNum) =>{
+    return total*nextNum;
+  }, 1);
 };
 
 const power = function() {
-	
+	return Math.pow(arguments[0], arguments[1]);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let arr = [];
+  for (let i = num; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr.reduce((total, nextNum) =>{
+    return total*nextNum;
+  }, 1);
 };
 
 // Do not edit below this line
