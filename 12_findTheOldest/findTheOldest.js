45211cdba9ff8163ed603a714de578b580471a47
Michael Ballif <mballifcodes@gmail.com>
2022-12-15 00:32:49 -0700

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let peopleSort = people.sort((a, b) => {
+        if ('yearOfDeath' in a) {
+            aYOD = a.yearOfDeath;
+        } else {
+            aYOD = new Date().getFullYear();
+        };
+        if ('yearOfDeath' in b){
+            bYOD = b.yearOfDeath
+        } else {
+            bYOD = new Date().getFullYear();
+        }
+        return ((aYOD-a.yearOfBirth) > (bYOD-b.yearOfBirth)) ? -1 : 1; }
+        );
+    return peopleSort[0];
 };
 
 // Do not edit below this line
