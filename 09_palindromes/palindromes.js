abe9784685bde9a16df6a282389e15aa9c366fc0
Michael Ballif <mballifcodes@gmail.com>
2022-12-14 23:44:23 -0700

    complete 09_palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let newPhrase = phrase.toLowerCase();
+    let arr = newPhrase.split('');
+    arr = arr.filter(letter => (/\w/).test(letter) ? true : false);
+    let backArr = [];
+    for (let i = arr.length-1; i >= 0; i--) {
+        backArr.push(arr[i]);
+    }
+    for (let i = 0; i <= arr.length; i++){
+        if (arr[i] != backArr[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
