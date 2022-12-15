fc4e28c84644aeaede6bec605214a13af220399d
Michael Ballif <mballifcodes@gmail.com>
2022-12-15 00:00:24 -0700

    complete 10_fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0){
+        return "OOPS";
+    }
+    let fib = [1, 1];
+    if (num <=2){
+        return 1;
+    }
+    for (let i = 2; i < num; i++){
+        fib.push(fib[i-2]+fib[i-1]);
+    }
+    return (fib[num-1]);
 };
 
 // Do not edit below this line
